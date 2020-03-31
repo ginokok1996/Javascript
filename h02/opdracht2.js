b85af3f4e2c7b1d7cc06d536c9ca5b2b7fd9833a
ginokok1996 <ginokok1996@hotmail.com>
2020-03-31 19:53:00 +0200

    hoofdstuk 2 toegevoegd

---
+++ h02/opdracht2.js
@@ -0,0 +1,15 @@
+//De kleuren aanpassen
+document.getElementById("bl1").style.backgroundColor = "lightblue";
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl4").style.backgroundColor = "red";
+
+//tekst locatie
+document.getElementById("bl1").style.textAlign = "center";
+document.getElementById("bl1").style.paddingTop = "10px";
+document.getElementById("bl2").style.textAlign = "center";
+document.getElementById("bl3").style.textAlign = "right";
+
+//blok locatie
+document.getElementById("bl2").style.marginTop = "10px";
+document.getElementById("bl3").style.marginTop = "30px";
